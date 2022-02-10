ba30ae0d0d94c5c4281e4723e31c27512380a9fc
aandinatha <aandinatha@gmail.com>
2022-02-10 21:14:13 +0700

    first commit

---
+++ application/controllers/akses1.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class akses1 extends CI_Controller
+{
+    public function okpunya()
+    {
+
+        $this->load->view('okpunya');
+    }
+}
